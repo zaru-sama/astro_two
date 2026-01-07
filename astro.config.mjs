e7a775e0898f2e5328b55f78dfd00e9fcd9c0e52
Alexander Busch <sasha.tishenin@gmail.com>
2026-01-07 22:47:49 +0100

    Configure Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://zaru-sama.github.io",
+  base: "/astro_two",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
